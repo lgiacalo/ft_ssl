01e9b60b9027ae16208ad8a4e5a130cd5a699398
Lena GIACALONE <lgiacalo@student.42.fr>
2020-01-09 17:49:41 +0100

    md5.c: norme ok

--- src/md5.c
+++ src/md5.c
@@ -1,7 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   md5.c.                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgiacalo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/15 05:23:35 by lgiacalo          #+#    #+#             */
+/*   Updated: 2019/05/31 15:57:47 by lgiacalo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_ssl.h"
 
-
-int	ind_block(int i)
+int				ind_block(int i)
 {
 	if (i < 16)
 		return (i);
@@ -15,78 +26,74 @@ int	ind_block(int i)
 unsigned int	func_block(unsigned int *block, int i)
 {
 	if (i < 16)
-		return(func_f(block[1], block[2] , block[3]));
+		return (func_f(block[1], block[2], block[3]));
 	else if (i < 32)
-		return(func_g(block[1], block[2], block[3]));
+		return (func_g(block[1], block[2], block[3]));
 	else if (i < 48)
-		return(func_h(block[1], block[2], block[3]));
-	return(func_i(block[1], block[2], block[3]));
+		return (func_h(block[1], block[2], block[3]));
+	return (func_i(block[1], block[2], block[3]));
 }
 
-void	MD5_transform(unsigned int	*block)
+void			md5_transform(unsigned int *block)
 {
-	ft_printf("\nFunction MD5_transform\n");
-
 	t_ssl			*ssl;
-	unsigned int	state_temp[4];
+	unsigned int	tmp[4];
 	unsigned int	func;
 	int				i;
 
+	ft_printf("\nFunction MD5_transform\n");
 	ssl = getssl();
-	ft_memcpy(state_temp, ssl->state, (sizeof(unsigned int) * 4));
+	ft_memcpy(tmp, ssl->state, (sizeof(unsigned int) * 4));
 	i = -1;
 	while (++i < 64)
 	{
-		func = state_temp[0] + func_block(state_temp, i) + block[ind_block(i)] + k[i];
-		state_temp[0] = state_temp[3];
-		state_temp[3] = state_temp[2];
-		state_temp[2] = state_temp[1];
-		state_temp[1] = state_temp[1] + rotate_left(func, r[i]);
+		func = tmp[0] + func_block(tmp, i) + block[ind_block(i)] + k[i];
+		tmp[0] = tmp[3];
+		tmp[3] = tmp[2];
+		tmp[2] = tmp[1];
+		tmp[1] = tmp[1] + rotate_left(func, r[i]);
 	}
 	i = -1;
 	while (++i < 4)
-		ssl->state[i] += state_temp[i];
+		ssl->state[i] += tmp[i];
 }
 
-void	gestion_last_block(char *block, unsigned int size)
+void			gestion_last_block(char *block, unsigned int size)
 {
-	ft_printf("Fonction gestion last block\n");
 	t_ssl	*ssl;
 	int		mod;
 	int		div;
 
+	ft_printf("Fonction gestion last block\n");
 	ssl = getssl();
-
 	mod = size % 64;
 	div = (size / 64) * 64;
 	ft_memcpy(ssl->buf, block + div, mod);
 	ft_memcpy(ssl->buf + mod, PADDING, 64 - mod);
-
 	if (mod >= 56)
 	{
-		MD5_transform((unsigned int *)(&(ssl->buf[0])));
+		md5_transform((unsigned int *)(&(ssl->buf[0])));
 		ft_bzero(ssl->buf, 64);
 		ft_memcpy(ssl->buf, PADDING + 1, 56);
 	}
 	ssl->size *= 8;
 	ft_memcpy(ssl->buf + 56, (char *)(&(ssl->size)), 8);
-	MD5_transform((unsigned int *)(&(ssl->buf[0])));
+	md5_transform((unsigned int *)(&(ssl->buf[0])));
 	reverse_block(ssl->state);
 }
 
-void	gestion_block(char *block, unsigned int size)
+void			gestion_block(char *block, unsigned int size)
 {
-	ft_printf("Fonction gestion block\n");
 	t_ssl			*ssl;
-	unsigned int 	i;
+	unsigned int	i;
 
+	ft_printf("Fonction gestion block\n");
 	ssl = getssl();
 	ssl->size += size;
-
 	i = 0;
 	while ((i + 63) < size)
 	{
-		MD5_transform((unsigned int *)(&(block[i])));
+		md5_transform((unsigned int *)(&(block[i])));
 		i += 64;
 	}
 }
