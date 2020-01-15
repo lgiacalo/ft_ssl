8bb1893d1f72bb8fe40395436702e0d497108b1f
Lena GIACALONE <lgiacalo@student.42.fr>
2020-01-15 23:52:15 +0100

    update display sha384/512

--- src/display.c
+++ src/display.c
@@ -13,13 +13,23 @@
 #include "ft_ssl.h"
 #include "ft_sha.h"
 
-void	print_hash(unsigned int state[], int ind)
+void	print_hash(unsigned int state[], int ind, int type)
 {
 	int	i;
 
 	i = -1;
-	while (++i < ind)
-		ft_printf("%.8x", state[i]);
+
+	if (type || getsha()->len_msg == 64)
+	{
+		while (++i < ind)
+			ft_printf("%.8x", state[i]);
+	}
+	else
+	{
+		ind -= (ind == 7) ? 1 : 0;
+		while (++i < ind)
+			ft_printf("%.16lx", getsha()->statee[i]);
+	}
 }
 
 void	display_hash(char *name)
@@ -30,17 +40,17 @@ void	display_hash(char *name)
 	ssl = getssl();
 	c = (ssl->opt & OPT_S) ? '"' : 0;
 	if (ssl->opt & OPT_Q)
-		print_hash(ssl->state, 4);
+		print_hash(ssl->state, 4, 1);
 	else if ((ssl->opt & OPT_P) || (ssl->opt & OPT_R))
 	{
-		print_hash(ssl->state, 4);
+		print_hash(ssl->state, 4, 1);
 		if (!(ssl->opt & OPT_P))
 			ft_printf(" %c%s%c", c, name, c);
 	}
 	else
 	{
 		ft_printf("MD5(%c%s%c)= ", c, name, c);
-		print_hash(ssl->state, 4);
+		print_hash(ssl->state, 4, 1);
 	}
 	ft_printf("\n");
 	clean_ssl();
@@ -56,17 +66,17 @@ void	display_hash256(char *name)
 	v = (sha->version) ? 8 : 7;
 	c = (sha->opt & OPT_S) ? '"' : 0;
 	if (sha->opt & OPT_Q)
-		print_hash(sha->state, v);
+		print_hash(sha->state, v, 0);
 	else if ((sha->opt & OPT_P) || (sha->opt & OPT_R))
 	{
-		print_hash(sha->state, v);
+		print_hash(sha->state, v, 0);
 		if (!(sha->opt & OPT_P))
 			ft_printf(" %c%s%c", c, name, c);
 	}
 	else
 	{
 		ft_printf("%s(%c%s%c)= ", sha->cmd, c, name, c);
-		print_hash(sha->state, v);
+		print_hash(sha->state, v, 0);
 	}
 	ft_printf("\n");
 	clean_sha();
